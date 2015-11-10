8ecdb1582d60f903291994d03849f9c5dfc31db1
de Francisco <fernando.defrancisco@cognizant.com>
2015-11-10 14:47:33 +0100

    Added routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/list', {
+      templateUrl: 'partials/list.html'
+    })
+    .when('/about', {
+      templateUrl: 'partials/about.html'
+    })
+    .otherwise({
+      redirectTo: '/list'
+    });
+});
