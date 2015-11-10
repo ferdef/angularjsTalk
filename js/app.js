d245e05c8447035588145ef892ec2305bbe25acf
Fernando de Francisco <fernando.defrancisco@cognizant.com>
2015-11-10 01:38:13 +0100

    Angular JS working example

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('myApp', []);
+
+app.controller("myAppCtrl", function($scope) {
+	$scope.todos = [];
+	
+	$scope.addItem = function(todo) {
+		todo.entrance = new Date();
+		$scope.todos.push(todo);
+		delete $scope.todo;	
+	};
+	
+	$scope.removeItem = function(itemPos) {
+		$scope.todos.splice(itemPos, 1);
+	}
+});
